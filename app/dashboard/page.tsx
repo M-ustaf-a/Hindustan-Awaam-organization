083980bfdb89a5c7612b97907a8633e25df372af
mustafa <mustafaalikhan757@gmail.com>
2024-09-03 23:00:40 +0530

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+
+import { ThreeDCardDemo } from '@/components/ThreeDCardDemo';
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <ThreeDCardDemo/>
+    </div>
+  )
+}
+
+export default Dashboard;
